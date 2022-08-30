d28522981a69e6757ac42a300c84554fe18dd8dc
DmitryG <dmitrygribchenko@mail.ru>
2022-08-30 21:44:37 +0300

    zad13

---
+++ zad13/Program.cs
@@ -0,0 +1,17 @@
+﻿int num = new Random().Next(1, 50000);
+int num3 = -1;
+int num2 = -1;
+int num1 = -1;
+int number = num;
+while (number > 0) {
+    num3 = num2;
+    num2 = num1;
+    num1 = number % 10;
+    number = number /10;
+ }
+if (num3 == -1) {
+    Console.WriteLine($"Третьей цифры в числе {num} нет");
+}
+else {
+    Console.WriteLine($"Третья цифра числа {num}: {num3}");
+}
