d655542284d72f8d4ff9e42cc84a0d9dff26399e
DmitryG <dmitrygribchenko@mail.ru>
2022-08-30 22:08:19 +0300

    zad15

---
+++ zad15/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Является ли день недели с номером {num} выхоным?");
+if (num == 1 || num == 2 || num == 3 || num == 4 || num == 5){
+    Console.WriteLine("Нет");
+}
+if (num == 6 || num == 7){
+    Console.WriteLine("Да");
+}
